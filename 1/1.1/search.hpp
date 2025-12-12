4da0171640e93f082ba7bb397e8a8e8daaa5af46
liza583 <liza30102006@mail.ru>
2025-12-12 22:26:47 +0300

    Create search.hpp

---
+++ 1/1.1/search.hpp
@@ -0,0 +1,9 @@
+#ifndef SEARCH_HPP
+#define SEARCH_HPP
+
+#include <string>
+
+// Функция для поиска вхождений строки в массиве строк
+int countOccurrences(const std::string& searchString, std::string* stringsToSearch, int count);
+
+#endif // SEARCH_HPP
