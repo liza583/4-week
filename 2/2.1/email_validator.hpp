050d8b58cb5019d1273476ec72a74176f3fbc1aa
liza583 <liza30102006@mail.ru>
2025-12-12 22:31:54 +0300

    Create email_validator.hpp

---
+++ 2/2.1/email_validator.hpp
@@ -0,0 +1,9 @@
+#ifndef EMAIL_VALIDATOR_HPP
+#define EMAIL_VALIDATOR_HPP
+
+#include <string>
+
+// Функция для проверки корректности адреса электронной почты
+bool isValidEmail(const std::string& email);
+
+#endif // EMAIL_VALIDATOR_HPP
