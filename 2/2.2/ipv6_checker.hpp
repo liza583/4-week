c0ae80d23fdce781098b513fd53ca76c254dd5eb
liza583 <liza30102006@mail.ru>
2025-12-12 22:34:04 +0300

    Create ipv6_checker.hpp

---
+++ 2/2.2/ipv6_checker.hpp
@@ -0,0 +1,9 @@
+#ifndef IPV6_CHECKER_HPP
+#define IPV6_CHECKER_HPP
+
+#include <string>
+
+// Функция для проверки правильности адреса IPv6
+bool isValidIPv6(const std::string& ip);
+
+#endif // IPV6_CHECKER_HPP
